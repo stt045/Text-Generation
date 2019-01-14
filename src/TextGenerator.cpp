44304160a6f1535698f0b159315e9ef04e7c0032
Steven Tran <steven@wireless-169-228-84-207.ucsd.edu>
2019-01-14 10:27:47 -0800

    Coded skeleton of textGenerator constructor

--- src/TextGenerator.cpp
+++ src/TextGenerator.cpp
@@ -7,7 +7,52 @@
 #include <iostream>
 
 TextGenerator::TextGenerator(const string & textDirectory) {
+	// psuedo random seed for std::rand
+	srand(time(NULL));
 
+	DIR * dir;
+	struct dirent * ent;
+	ifstream infile;
+	string line;
+	string output;
+	vector<string tokens>;
+
+	// Opening the directory
+	if((dir = opendir(textDirectory.c_str())) != nullptr) {
+    	int i = 0;
+		 
+		// Loop to open individual files 
+    	while((ent = readdir(dir)) != nullptr) {
+    		i++;
+    		// Skips the current and parent directory file in the current directory
+    		if(i < 2) {
+    			continue;
+    		}
+
+    		char str[BUFSIZ]; // may need to update this size
+    		strcat(str, textDirectory.c_str());
+    		strcat(str, "/");
+    		strcat(str, end->d_name);
+
+    		// Opening individual files
+    		infile.open(str);
+    		if(!infile) {
+ 				closedir(dir);
+ 				cerr << "Couldn't Open File!" << str << endl;
+ 				exit(1);
+    		}
+
+    		// Remove Unwanted Charactor! 
+    		while() {
+
+    		}
+
+    		infile.close();
+
+    		// Tokenize text into individual words and punctuations
+    	}
+
+	}
 }
 
 string TextGenerator::generateNextWord(const string & prevWord) {
@@ -31,7 +76,9 @@ vector<string> TextGenerator::tokenize(string & input) {
 }
 
 string TextGenerator::genRand(vector<string> & v) {
-
+	int total = v.size();
+	int index = rand() % total;
+	return v[index];
 }
 
 // Destructor
